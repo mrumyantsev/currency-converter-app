9acb1c7c013a3112d7b9a75448857c75bda48a68
Mikhail Rumyantsev <mi.rumyantsev.2020@gmail.com>
2023-09-25 06:45:53 +0300

    feat: add cors resolution to allow frontend requests

--- internal/pkg/http-server/http-server.go
+++ internal/pkg/http-server/http-server.go
@@ -33,6 +33,7 @@ func New(cfg *config.Config, memStorage *memstorage.MemStorage) *HttpServer {
 
 	mux.Handle("/currencies.json", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.WriteHeader(http.StatusOK)
 		server.getCurrencies(w, r)
 	}))
